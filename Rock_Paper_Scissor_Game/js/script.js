0c1d3428aaa2481e67a7e44bc83e85f37fee770d
deramworld <you@example.com>
2024-03-04 23:26:52 +0530

    first commit

---
+++ Rock_Paper_Scissor_Game/js/script.js
@@ -0,0 +1,17 @@
+function goToHomePage() {
+    window.location.href = 'index.html'; 
+}
+
+function showPopUp() {
+    var rulesPopup = document.getElementById('rulesPopup');
+    rulesPopup.style.display = 'block';
+}
+
+function hidePopUp() {
+    var rulesPopup = document.getElementById('rulesPopup');
+    rulesPopup.style.display = 'none';
+}
+
+function goToNext() {
+    window.location.href = 'final.html'; 
+}
